83594ffbc4be51a83d0378b0097ceb6da2f8cd31
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-10-16 12:04:25 +0100

    description added

--- builtin-main.c
+++ builtin-main.c
@@ -1,16 +1,21 @@
 #include "main.h"
 
+/**
+ * handle_builtins - function that executes builtin commands
+ * @tokens: pointer to an array of strings
+ * @token_count: pointer to counter for tokens
+*/
 
 void handle_builtins(char *tokens[], int *token_count)
 {
 int i = 0;
 Builtin builtins[] = {
-    {"exit", handle_exit},
-    {"cd", handle_cd},
-    {"env", handle_env},
-    {"setenv", handle_setenv},
-    {"unsetenv", handle_unsetenv},
-    {NULL, NULL}
+{"exit", handle_exit},
+{"cd", handle_cd},
+{"env", handle_env},
+{"setenv", handle_setenv},
+{"unsetenv", handle_unsetenv},
+{NULL, NULL}
 };
 
 while (builtins[i].builtin_name != NULL)
